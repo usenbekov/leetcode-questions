3c1f24b03e5c3c7735aee32e97c4b9e5f8408a09
usenbekov <altosh.mail@gmail.com>
2020-04-06 18:02:55 +0600

    fix: indentation

--- algorithms/49. Group Anagrams.js
+++ algorithms/49. Group Anagrams.js
@@ -28,30 +28,30 @@ var groupAnagrams = function(strs) {
 
 // O (N K log(K)) -> N: strs.length, K: max(len(str in strs))
 var solution1 = (strs) => {
-const map = new Map();
-for (const str of strs) {
-  const key = str.split('').sort().join('');
-  if (!map.has(key)) map.set(key, []);
-  map.get(key).push(str);
-}
-return Array.from(map.values());
+  const map = new Map();
+  for (const str of strs) {
+    const key = str.split('').sort().join('');
+    if (!map.has(key)) map.set(key, []);
+    map.get(key).push(str);
+  }
+  return Array.from(map.values());
 }
 
 // O (N K)
 var solution2 = (strs) => {
-const charStart = 'a'.charCodeAt(0);
-const groupedAnagrams = new Map();
-for (const str of strs) {
-  const charCount = new Array(26).fill(0);
-  for (let i = 0; i < str.length; i++) {
-    charCount[str.charCodeAt(i) - charStart]++;
+  const charStart = 'a'.charCodeAt(0);
+  const groupedAnagrams = new Map();
+  for (const str of strs) {
+    const charCount = new Array(26).fill(0);
+    for (let i = 0; i < str.length; i++) {
+      charCount[str.charCodeAt(i) - charStart]++;
+    }
+    
+    let key = charCount.toString();
+    if (!groupedAnagrams.has(key)) groupedAnagrams.set(key, []);
+    groupedAnagrams.get(key).push(str);
   }
-  
-  let key = charCount.toString();
-  if (!groupedAnagrams.has(key)) groupedAnagrams.set(key, []);
-  groupedAnagrams.get(key).push(str);
-}
-return Array.from(groupedAnagrams.values());
+  return Array.from(groupedAnagrams.values());
 }
 
 
